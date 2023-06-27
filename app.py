0aea531d9fa5af1f251bd211988ce9de950c9ff7
kplin-carrefour <137925375+kplin-carrefour@users.noreply.github.com>
2023-06-27 18:26:20 -0300

    Publish e Execute (teste)

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import secrets
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.post('/publish')
+def publish():
+    return ""
+
+@app.post('/execute')
+def execute():
+    root = request.json.get('root')
+    journeyVersionId = root.journeyId
+    subscriberKey = root.keyValue
+
+    grupoControle = secrets.randbelow(10) >= 9 #10%
+
+    #upsert in DataExtension
+
+    if grupoControle
+        return { branchResult: "grupo-controle", j: journeyVersionId, s: subscriberKey }
+    else
+        return { branchResult: "audiencia", j: journeyVersionId, s: subscriberKey }
